ea1ccb1ea2ef35a0e16138b00bb4624ceeb95023
Christian Oder <myself5@carbonrom.org>
2023-11-13 16:13:29 +0000

    mt6789-common: Build disabled VBMeta image
    Change-Id: I22e39b899215dde223fe4499d86d770579d305cf

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -67,5 +67,9 @@ TARGET_BOARD_PLATFORM := mt6789
 BOARD_HAS_MTK_HARDWARE := true
 BOARD_VENDOR := samsung
 
+# Verified Boot
+BOARD_AVB_ENABLE := true
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
+
 # Inherit the proprietary files
 include vendor/samsung/mt6789-common/BoardConfigVendor.mk
